8a8a2cf6c66942848b77aebc4e656bf93f0a9732
Etumi <etumi12@gmail.com>
2020-04-06 14:57:42 -0400

    updates

--- app.py
+++ app.py
@@ -10,9 +10,12 @@ def homepage():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start>/<end>"
+        f"/api/v1.0/start;<br/>"
+        f"/api/v1.0/start;/end;"
     )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
 
 
 if __name__ == "__main__":
